32e3fe0609a2d5e7f0fba9c489b691816f955dc8
e-baron <raphael.baroni@vinci.be>
2021-10-08 16:37:18 +0200

    update : oversized fontawesome bug correction ?

--- src/components/main-layout.js
+++ src/components/main-layout.js
@@ -12,6 +12,11 @@ import SEO from "./seo/seo.js";
 import "../scss/main.scss";
 import Scroll from "./scroll/scroll";
 
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+// Fix huge icon flash: https://github.com/FortAwesome/react-fontawesome/issues/234
+config.autoAddCss = false;
+
 const MainLayout = ({
   children,
   navbarExtraStyles,
@@ -49,46 +54,42 @@ const MainLayout = ({
   console.log("main-layout:", navbarExtraStyles, featuredImage, frontmatter);
 
   return (
-    
-      <div className="master">
-        {/* Dealing with Meta Tags : page title.... 
+    <div className="master">
+      {/* Dealing with Meta Tags : page title.... 
       Use the SEO component*/}
-        {/* <Helmet>{pageTitle && <title>{pageTitle}</title>}</Helmet> */}
-        <SEO
-          title={pageTitle}
-          {...(frontmatter ? { description: frontmatter.description } : {})}
-          language="fr"
-        />
+      {/* <Helmet>{pageTitle && <title>{pageTitle}</title>}</Helmet> */}
+      <SEO
+        title={pageTitle}
+        {...(frontmatter ? { description: frontmatter.description } : {})}
+        language="fr"
+      />
 
-        <Header
-          siteTitle={data.site.siteMetadata.title}
-          menuLinks={data.site.siteMetadata.menuLinks}
-          {...(navbarExtraStyles
-            ? { navbarExtraStyles: navbarExtraStyles }
-            : {})}
-          {...(headerImage ? { headerImage: headerImage } : {})}
-        />
+      <Header
+        siteTitle={data.site.siteMetadata.title}
+        menuLinks={data.site.siteMetadata.menuLinks}
+        {...(navbarExtraStyles ? { navbarExtraStyles: navbarExtraStyles } : {})}
+        {...(headerImage ? { headerImage: headerImage } : {})}
+      />
 
-        <main className="main">
-          {featuredImage && (
-            <div>
-              <Section className="pt-3">
-                <SectionHeader className="section__header--left">
-                  {pageTitle}
-                </SectionHeader>
-                <Content className="vh-50">
-                  <Image name={featuredImage} />{" "}
-                </Content>
-              </Section>
-            </div>
-          )}
-          {children}
-        </main>
+      <main className="main">
+        {featuredImage && (
+          <div>
+            <Section className="pt-3">
+              <SectionHeader className="section__header--left">
+                {pageTitle}
+              </SectionHeader>
+              <Content className="vh-50">
+                <Image name={featuredImage} />{" "}
+              </Content>
+            </Section>
+          </div>
+        )}
+        {children}
+      </main>
 
-        <Footer siteMetaData={data.site.siteMetadata}></Footer>
-        <Scroll showBelow={250} />
-      </div>
-  
+      <Footer siteMetaData={data.site.siteMetadata}></Footer>
+      <Scroll showBelow={250} />
+    </div>
   );
 };
 
