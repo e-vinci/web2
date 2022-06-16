6bb9f3f47204ab1edb86edd4e33fdbc24ddb1420
e-baron <raphael.baroni@vinci.be>
2022-06-16 11:48:05 +0200

    update : trying to provide everything for mdx formating

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,11 +1,4 @@
-/*import React from "react";
-import ProjectDataProvider from "./src/components/context/projects/project-data-context";
-
-export const wrapRootElement = ({ element }) => (
-  <ProjectDataProvider>{element}</ProjectDataProvider>
-);
-*/
-
 import wrapWithProvider from "./src/utils/auths/wrap-with-provider";
 
 export const wrapRootElement = wrapWithProvider;
+
--- src/utils/auths/wrap-with-provider.js
+++ src/utils/auths/wrap-with-provider.js
@@ -2,14 +2,23 @@ import React from "react";
 import { PublicClientApplication } from "@azure/msal-browser";
 import { MsalProvider } from "@azure/msal-react";
 import { msalConfig } from "./authConfig";
-import { CustomNavigationClient } from "../navigation/navigation-client";
+import { MDXProvider } from "@mdx-js/react";
+import CodeBlock from "./src/components/codeblock/codeblock";
 
 const msalInstance = new PublicClientApplication(msalConfig);
 
-export default ({ element }) => {
-  // The next 2 lines are optional. This is how you configure MSAL to take advantage of the router's navigate functions when MSAL redirects between pages in your app
-  //const navigationClient = new CustomNavigationClient();
-  //msalInstance.setNavigationClient(navigationClient);
-
-  return <MsalProvider instance={msalInstance}>{element}</MsalProvider>;
+// Deal with code snippet in MD(X) files
+const component = {
+  pre: CodeBlock,
 };
+
+
+export default ({ element }) => {
+  return (<MsalProvider instance={msalInstance}>
+    <MDXProvider components={component}>{element}
+    </MDXProvider>
+    </MsalProvider>);
+};
+
+
+
