413a275789517ae84475ecd9381e28d466cef6f6
e-baron <raphael.baroni@vinci.be>
2022-06-26 14:24:06 +0200

    Updates

---
+++ component---src-pages-logout-js-abcabd684f529082dc21.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmyjscourse=self.webpackChunkmyjscourse||[]).push([[321],{35354:function(e,t,n){n.r(t);var u=n(67294),c=n(8709),s=n(1597),a=n(69233);t.default=function(){var e=(0,c.Fp)().instance;return(0,u.useEffect)((function(){e.logoutRedirect(),(0,s.navigate)("/")})),u.createElement(a.Z,null,u.createElement("h3",{className:"pl-3"},"Logout"))}}}]);
+//# sourceMappingURL=component---src-pages-logout-js-abcabd684f529082dc21.js.map
