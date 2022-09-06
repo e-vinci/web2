be285edebe7a2f95d5f06a06fa48052b86be3e50
e-baron <raphael.baroni@vinci.be>
2022-09-06 13:12:04 +0200

    update : cosmétique

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,17 +1,20 @@
-import React from "react";
-import wrapWithProvider from "./src/utils/auths/wrap-with-provider";
-import { siteMetadata, plugins } from "./gatsby-config";
+import React from 'react';
+import wrapWithProvider from './src/utils/auths/wrap-with-provider';
+import { siteMetadata, plugins } from './gatsby-config';
 
 const { options: i18nPluginOptions } = plugins.find(
   (plugin) => plugin.resolve === `gatsby-plugin-i18n`
 );
 
 export const wrapRootElement = wrapWithProvider;
-export const onServiceWorkerUpdateReady = () => window.location.reload(true);
+
+export const onServiceWorkerUpdateReady = () => {
+  window.location.reload(true);
+};
 
 export const onClientEntry = () => {
   if (i18nPluginOptions.prefixDefault) {
-    if (window.location.pathname === "/") {
+    if (window.location.pathname === '/') {
       window.location.pathname = siteMetadata.languages.defaultLangKey;
     }
   }
